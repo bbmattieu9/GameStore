bceb6acea384e60ce3a2cd54fdf83003e377381a
Babatunde Mattieu <bbmattieu9@gmail.com>
2026-09-23 23:07:07 +0100

    Add games endpoint
    Add GameDto data model, in-memory game data, and GET /games API route.

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,3 +1,5 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,27 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+List<GameDto> games = [
+    new (1, 
+        "Street Fighter II", 
+        "Fighting", 
+        19.99M, 
+        new DateOnly(1992, 7, 15)),
+    new  (
+        2,
+        "Elden Ring",
+        "Platformer",
+        19.99M,
+        new DateOnly(1991, 9, 12)
+        ),
+    new (
+        3,
+        "Super Mario Wonder",
+        "RPG",
+        69.30M,
+        new DateOnly(1995, 9,  20))
+];
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -13,6 +36,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/games", () => Results.Ok(games));
 
 app.Run();
 
